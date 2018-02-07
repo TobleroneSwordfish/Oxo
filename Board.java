d139e9de9bad310ba814798bac8b37808013fce5
Max Whale <maxwhale99@gmail.com>
2018-02-07 11:40:22 +0000

    Add position method

--- Board.java
+++ Board.java
@@ -5,6 +5,36 @@ class Board
         //stuff
     }
 
+    public Position position(String s)
+    {
+        int r, c;
+        if (s.length() != 2) return null;
+        switch (s.charAt(0))
+        {
+            case 'a':
+                r = 0; break;
+            case 'b':
+                r = 1; break;
+            case 'c':
+                r = 2; break;
+            default:
+                return null;
+        }
+        switch (s.charAt(1))
+        {
+            case '1':
+                c = 0; break;
+            case '2':
+                c = 1; break;
+            case '3':
+                c = 2; break;
+            default:
+                return null;
+        }
+
+        return new Position(r, c);
+    }
+
     Player cells[][];
     Player currentPlayer;
     Board()
